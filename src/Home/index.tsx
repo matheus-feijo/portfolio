65e4bd124f205d364a344580e2fc9bb163341eae
matheus <mat.feijo2506@gmail.com>
2022-08-25 11:31:03 -0400

    init portfolio

---
+++ src/Home/index.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+
+export function Home() {
+
+
+    return (
+        <div>
+            <p className="text-5xl font-bold">Eu sou o Matheus Feijó</p>
+            <p>Desenvolvedor frontEnd</p>
+
+        </div>
+    )
+}
