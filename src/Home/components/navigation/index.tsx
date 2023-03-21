66c4bf143e707ffebac5f84368ed861e287660a2
Matheus Feijó <matheus.feijo@sydy.com.br>
2023-03-21 13:46:55 -0400

    fix apresentacao com animação

--- src/Home/components/navigation/index.tsx
+++ src/Home/components/navigation/index.tsx
@@ -1,66 +1,53 @@
 import { Fragment } from "react";
 import "./style.css";
 
-
-
-
-
 export function Navigation() {
-
-    const toggleScrool = () => {
-        if (window.screen.availWidth <= 800) {
-            return 1200;
-        }
-
-        return 1400
+  const toggleScrool = () => {
+    if (window.screen.availWidth <= 800) {
+      return 1200;
     }
 
+    return 1400;
+  };
 
-    return (
-        <Fragment>
-            <div className="navigation-pc">
-                <ul className="justify-around flex items-center h-[8vh]">
-                    <li>
-                        <button onClick={(e) => {
-                            e.preventDefault();
-                            window.scrollTo({ behavior: 'smooth', top: 0 })
-                        }}>
-                            Sobre
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={(e) => {
-                            e.preventDefault();
-                            window.scrollTo({ behavior: 'smooth', top: 700 })
-                        }}>
-                            Habilidades
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={(e) => {
-                            e.preventDefault();
-                            window.scrollTo({ behavior: 'smooth', top: toggleScrool() })
-                        }}>
-                            Projetos
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={(e) => {
-                            e.preventDefault();
-                            window.scrollTo({ behavior: "smooth", top: 10000 })
-                        }}>
-                            Experiências
+  return (
+    <Fragment>
+      <div className="navigation-pc">
+        <ul className="justify-around flex items-center h-[8vh]">
+          <li>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                window.scrollTo({ behavior: "smooth", top: 0 });
+              }}
+            >
+              Sobre
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                window.scrollTo({ behavior: "smooth", top: 700 });
+              }}
+            >
+              Habilidades
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                window.scrollTo({ behavior: "smooth", top: 10000 });
+              }}
+            >
+              Experiências
+            </button>
+          </li>
+        </ul>
+      </div>
 
-                        </button>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="navigation-mobile">
-            </div>
-
-        </Fragment>
-
-
-    )
+      <div className="navigation-mobile"></div>
+    </Fragment>
+  );
 }
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,18 +2,15 @@ import { Fragment } from "react";
 import { Apresentation } from "./components/apresentation";
 import { Experiences } from "./components/experiences";
 import { Navigation } from "./components/navigation";
-import { Projects } from "./components/projects";
 import { Skills } from "./components/skills";
 
 export function Home() {
-
-    return (
-        <Fragment>
-            <Navigation />
-            <Apresentation />
-            <Skills />
-            <Projects />
-            <Experiences />
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Navigation />
+      <Apresentation />
+      <Skills />
+      <Experiences />
+    </Fragment>
+  );
 }
