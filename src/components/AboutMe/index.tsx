d2625cfed1f59048b92b05aa5d7bda9107629240
Matheus Feijó <mat.feijo2506@gmail.com>
2024-04-11 12:05:14 -0400

    FIX/ TAMANHO TELA NOTEBOOK

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -7,17 +7,17 @@ export function AboutMe() {
       <div className="w-screen pl-24 pr-24 pt-24 flex gap-2 justify-center items-start flex-wrap mobile:pl-0 mobile:pr-0 mobile:pt-1 ">
         <div className="grid grid-rows-principal justify-items-center items-end w-2/4 mobile:w-full mobile:grid-rows-mobile notebook:grid-rows-mobile">
           <div>
-            <h1 className="text-vermelhoPrincipal leading-relaxed whitespace-nowrap overflow-hidden select-none animate-wiggle text-5xl pr-1 mobile:text-2xl notebook:text-3xl sm:leading-relaxed">
+            <h1 className="text-vermelhoPrincipal leading-relaxed whitespace-nowrap overflow-hidden select-none animate-wiggle text-5xl pr-1 mobile:text-2xl  sm:leading-relaxed">
               Matheus Feijó
             </h1>
           </div>
 
-          <p className="text-white text-xl text-justify animate-more-details mobile:text-lg notebook:text-base">
+          <p className="text-white text-xl text-justify animate-more-details mobile:text-lg">
             Desenvolvedor de Sotware.
           </p>
 
           <div className="flex flex-col gap-5 justify-center items-center animate-habilidades">
-            <h1 className="text-vermelhoPrincipal text-5xl mobile:text-2xl notebook:text-3xl">
+            <h1 className="text-vermelhoPrincipal text-5xl mobile:text-2xl">
               Habilidades
             </h1>
 
@@ -25,7 +25,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <g fill="#61DAFB">
                     <circle cx="64" cy="47.5" r="9.3"></circle>
@@ -36,7 +36,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <path
                     fill="#83CD29"
@@ -47,7 +47,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <g fillRule="evenodd" clipRule="evenodd">
                     <path
@@ -69,7 +69,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <path fill="#fff" d="M22.67 47h99.67v73.67H22.67z"></path>
                   <path
@@ -83,7 +83,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <path
                     fill="#F0DB4F"
@@ -99,7 +99,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <path
                     fill="#00618A"
@@ -119,7 +119,7 @@ export function AboutMe() {
               <ul>
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <path d="M30.2 45.9h24.1v1.9H32.4v14.4H53v1.9H32.4v15.8h22.2v1.9H30.2V45.9zm26.3 0h2.6l11.4 15.8L82 45.9l15.8-20-26 37.5 13.4 18.4h-2.7L70.4 65 58.2 81.8h-2.6l13.5-18.4-12.6-17.5zm29.7 1.9v-1.9h27.5v1.9H101v34h-2.2v-34H86.2zM0 45.9h2.7l38.2 56.8-15.8-20.9L2.3 48.6l-.1 33.2H0zm113.5 33.4c.5 0 .8-.3.8-.8s-.3-.8-.8-.8-.8.3-.8.8.4.8.8.8zm2.2-2.1c0 1.3 1 2.2 2.4 2.2 1.5 0 2.4-.9 2.4-2.5v-5.5h-1.2v5.5c0 .9-.4 1.3-1.2 1.3-.7 0-1.2-.4-1.2-1.1h-1.2zm6.3-.1c.1 1.4 1.2 2.3 3 2.3s3-.9 3-2.4c0-1.2-.7-1.8-2.2-2.2l-.9-.2c-1-.2-1.4-.6-1.4-1.1 0-.7.6-1.2 1.6-1.2.9 0 1.5.4 1.6 1.2h1.2c-.1-1.3-1.2-2.2-2.8-2.2-1.7 0-2.8.9-2.8 2.3 0 1.1.6 1.8 2 2.1l1 .2c1 .2 1.5.6 1.5 1.2 0 .7-.7 1.2-1.7 1.2s-1.8-.5-1.9-1.2H122z"></path>
                 </svg>
@@ -128,7 +128,7 @@ export function AboutMe() {
               <ul data-tooltip-target="tooltip-default">
                 <svg
                   viewBox="0 0 128 128"
-                  className="w-10 h-10 mobile:w-8 mobile:h-8 notebook:w-8 notebook:h-8"
+                  className="w-10 h-10 mobile:w-8 mobile:h-8"
                 >
                   <path
                     d="M31.393 97.211c-.375 0-.75.108-1.074.322-.325.215-.621.584-.621 1.057 0 .063-.037.203.084.445l-.03-.076 6.57 21.318c.117.397.395.701.71.875.314.174.662.238.998.238.714 0 1.495-.34 1.734-1.105l.002-.002 6.565-21.396v-.076c0 .103.03.042.03-.188 0-.48-.306-.837-.636-1.06a2 2 0 00-1.12-.352c-.498 0-1.07.3-1.263.875l-.002.008-5.312 17.727-5.332-17.697c-.078-.313-.287-.571-.531-.717s-.514-.195-.772-.195zm52.336 1.1c-1.04 0-1.914.794-1.914 1.789 0 .995.861 1.822 1.914 1.822 1.033 0 1.853-.842 1.853-1.822 0-.98-.834-1.79-1.853-1.79zm-15.406 6.826c-3.57 0-6.508 2.855-6.508 6.287v3.521c0 1.923.765 3.572 2.065 4.71 1.3 1.137 3.115 1.767 5.197 1.767 1.538 0 2.792-.343 3.688-.81.447-.234.805-.497 1.07-.782.265-.285.46-.598.46-.984 0-.356-.138-.664-.36-.942-.223-.277-.57-.533-1.02-.533-.325 0-.577.138-.795.272s-.422.281-.67.424c-.495.285-1.152.562-2.31.562-1.29 0-2.366-.366-3.108-.984-.742-.619-1.174-1.48-1.174-2.604v-.664h6.61c.703 0 1.484.002 2.156-.428.672-.429 1.08-1.284 1.08-2.588 0-3.61-3.011-6.224-6.38-6.224zm24.793 0c-1.898 0-3.294.613-4.19 1.535-.895.922-1.28 2.121-1.28 3.242 0 1.43.577 2.451 1.404 3.108.826.656 1.855.987 2.836 1.283.98.296 1.92.56 2.552.949.633.39.977.814.977 1.705 0 .48-.145.862-.535 1.174-.39.312-1.083.56-2.205.56-1.329 0-2.075-.331-2.616-.666-.27-.167-.486-.34-.709-.494-.223-.154-.475-.318-.826-.318-.422 0-.735.26-.926.531-.19.271-.298.58-.298.912 0 .411.199.771.484 1.086.285.315.672.598 1.146.844.949.491 2.254.834 3.807.834 1.686 0 3.071-.412 4.05-1.21.98-.796 1.515-1.98 1.515-3.347 0-1.544-.56-2.653-1.387-3.386-.827-.734-1.872-1.112-2.867-1.416-.995-.305-1.953-.542-2.596-.877-.644-.34-.952-.65-.952-1.37 0-.388.157-.876.55-1.26.394-.384 1.034-.696 2.096-.696.85 0 1.48.198 1.985.405.252.103.472.209.68.299.207.09.398.177.67.177.49 0 .827-.324 1.029-.627.202-.302.32-.601.32-.941 0-.425-.267-.727-.562-.955-.295-.228-.666-.41-1.096-.568-.86-.316-1.956-.518-3.057-.518zm-44.959.187c-.826 0-1.537.521-1.537 1.287v8.399c0 3.4 2.891 6.412 6.476 6.412 3.525 0 6.475-2.977 6.475-6.412v-8.43c0-.384-.208-.718-.486-.93s-.635-.326-1.02-.326a1.73 1.73 0 00-1.025.32c-.285.208-.512.54-.512.936v8.43c0 1.793-1.56 3.525-3.431 3.525-1.902 0-3.432-1.763-3.432-3.525v-8.399c0-.397-.218-.705-.488-.923s-.625-.364-1.02-.364zm35.572 0c-.387 0-.746.1-1.03.315s-.476.566-.476.941v15.475c0 1.162-.08 2.138-.336 2.73-.256.594-.566.858-1.43.858-.753 0-1.38.657-1.38 1.445 0 .328.107.685.375.965.267.28.682.447 1.164.447 1.63 0 2.876-.722 3.619-1.896.743-1.174 1.033-2.748 1.033-4.549V106.58c0-.399-.228-.746-.518-.951a1.753 1.753 0 00-1.021-.305zm-15.439 2.51c2.048 0 3.432 1.716 3.432 3.463 0 .19-.021.316-.041.375s-.02.054-.051.074c-.061.04-.377.121-.98.121h-5.792v-.695c0-1.829 1.527-3.338 3.432-3.338zm9.75 9.631c-1.109 0-1.945.917-1.945 1.978 0 1.02.824 1.948 1.945 1.948 1.06 0 1.916-.9 1.916-1.948 0-1.087-.87-1.978-1.916-1.978z"
@@ -146,7 +146,7 @@ export function AboutMe() {
 
         <div className="flex flex-col items-center gap-5">
           <img
-            className="w-4/5 h-4/5 max-w-[25rem] max-h-[25rem] rounded-full mobile:w-32 mobile:h-32 notebook:w-64 notebook:h-64"
+            className="w-4/5 h-4/5 max-w-[20rem] max-h-80 rounded-full mobile:w-32 mobile:h-32 notebook:w-64 notebook:h-64"
             src={foto}
             alt="Foto do deselvovedor referenciado no site"
           />
