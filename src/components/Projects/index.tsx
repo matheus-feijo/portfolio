1a35601278dfcc206c5064aae3145aebfb58ca38
Matheus Feijó <mat.feijo2506@gmail.com>
2024-04-11 09:20:14 -0400

    feat/ tela de projetos

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -7,6 +7,9 @@ import {
   CardTitle,
 } from "../../components-ui/ui/card";
 import rickAndMortyCapa from "../../assets/capa-rick-and-morty.jpg";
+import { Button } from "../../components-ui/ui/button";
+import { GithubLogo } from "@phosphor-icons/react/dist/ssr";
+import { Globe } from "@phosphor-icons/react";
 
 export function Projects() {
   return (
@@ -28,40 +31,30 @@ export function Projects() {
               alt="imagem ilustrativa de Rick and Morty"
             />
           </CardContent>
-          <CardFooter className="flex justify-evenly">
-            <button className="border border-vermelhoPrincipal rounded-md h-8 w-20">
-              Github
-            </button>
+          <CardFooter className="flex justify-center gap-2">
+            <Button className="p-0">
+              <a
+                rel="noreferrer"
+                href="https://github.com/matheus-feijo/rick-and-morty-app"
+                target="_blank"
+                className="flex items-center justify-center gap-1 w-24 h-full"
+              >
+                <GithubLogo size={16} />
+                Github
+              </a>
+            </Button>
 
-            <button className="border border-vermelhoPrincipal rounded-md h-8 w-20">
-              Site
-            </button>
-          </CardFooter>
-        </Card>
-
-        <Card className="w-96 h-96">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-
-        <Card className="w-96 h-96">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
+            <Button className="p-0">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://rick-and-morty-app.matheus-feijo.vercel.app/"
+                className="flex items-center justify-center gap-1 w-24 h-full"
+              >
+                <Globe size={16} />
+                Site
+              </a>
+            </Button>
           </CardFooter>
         </Card>
       </div>
